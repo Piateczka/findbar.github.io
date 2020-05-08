3a05136cc81be0ce4d2074bc5a1850d25611d3bf
maciej_szostak <maciej.szostak@domd.pl>
2020-05-08 14:28:34 +0200

    Change display list settings

--- findBar.js
+++ findBar.js
@@ -1,4 +1,5 @@
 var itemsList = []
+var ul = $("#elements")
 function getToken(){
 
     let client_id = '51213679698c459cb5d247a8a3a3eca3';
@@ -6,21 +7,21 @@ function getToken(){
     window.location.replace(`https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`);
 
   };
-  function handleResponseError(xhr, ajaxOptions, thrownError){
+  function handleResponseError(jqXHR, textStatus, errorThrown){
     alert(xhr.status);
     alert(thrownError);
     var ul = $("#elements");
-    ul.append( "<li>WError in access to data</li>" );
+    ul.append( "<li>Error in access to data "+jqXHR.responseText+"</li>" );
   }
 
   function handleResponse(data){
     itemsList =  data.albums.items;
-    var ul = $("#elements");
-    for(var i=0;i<itemsList.length;i++){
-        var elem = ul.append( "<li>"+itemsList[i].name+"</li>" );
-   }
+//     var ul = $("#elements");
+//     for(var i=0;i<itemsList.length;i++){
+//         var elem = ul.append( "<li>"+itemsList[i].name+"</li>" );
+//    }
    if(itemsList.length<=0){
-    ul.append( "<li>Not found</li>" );
+    ul.append( "<li>Any albums was not found</li>" );
    }
   }
 
@@ -33,7 +34,7 @@ function getToken(){
         },
         async: false,
         success: handleResponse,
-        error: handleResponseError (xhr, ajaxOptions, thrownError)
+        error: function (jqXHR, textStatus, errorThrown) { handleResponseError(jqXHR, textStatus, errorThrown) }
     });
 };
 
@@ -49,16 +50,19 @@ function getToken(){
  })();
  function search() {
     var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
+    input = $("#myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("elements");
+    var result = itemsList.filter(item=>item.name.indexOf(filter)!==-1)
+    console.log(result)
     li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        txtValue = li[i].innerHTML
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
+    for (i = 0; i < result.length; i++) {
+        var elem = ul.append( "<li>"+itemsList[i].name+"</li>" );
+
+        // txtValue = itemsList[i].name
+        // if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        //     li[i].style.display = "";
+        // } else {
+        //     li[i].style.display = "none";
+        // }
     }
 }
